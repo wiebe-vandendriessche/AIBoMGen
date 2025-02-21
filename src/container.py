2f119accc1b1ba923eba56ea591825e77f63ad2e
Wiebe Vandendriessche <wiebe.vandendriessche@ugent.be>
2025-02-21 19:31:28 +0100

    Added example with externally mounted user data for training (basic mnist example)

--- src/container.py
+++ src/container.py
@@ -5,7 +5,7 @@ from docker.types import LogConfig
 
 client = docker.from_env()
 
-def run_user_docker_container(user_dockerfile_abs, user_dockerfile, output_folder, project_root):
+def run_user_docker_container(user_dockerfile_abs, user_dockerfile, output_folder_abs, mounted_data_abs, project_root):
     print(f"Building Docker image from {user_dockerfile_abs}...")
     try:
         image, _ = client.images.build(
@@ -18,12 +18,17 @@ def run_user_docker_container(user_dockerfile_abs, user_dockerfile, output_folde
         return None
 
     print("Starting Docker container with user's training code...")
+
+    volumes = {
+        output_folder_abs: {'bind': '/output', 'mode': 'rw'},
+        mounted_data_abs: {'bind': '/user_mounted_data/mnist_data', 'mode': 'ro'}  # Mount dataset as read-only
+    }
+
     try:
         container = client.containers.run(
             image.id,
-            name="user-ai-container",
             remove=False,
-            volumes={output_folder: {'bind': '/output', 'mode': 'rw'}},
+            volumes=volumes,
             detach=True,
             log_config=LogConfig(type="json-file"),  # Set a readable log driver
             runtime="nvidia"  # Enable GPU (requires NVIDIA runtime installed)
--- src/main.py
+++ src/main.py
@@ -3,15 +3,18 @@ import container
 import generator
 
 def main():
-    user_dockerfile = "test2/Dockerfile"
+    user_dockerfile = "test3/Dockerfile"
     output_folder = "output"
+    mounted_data = "user_mounted_data/mnist_data"
     project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
     # Construct the absolute path to the Dockerfile
     user_dockerfile_abs = os.path.join(project_root, user_dockerfile)
     output_folder_abs = os.path.join(project_root, output_folder)
 
-    container_id = container.run_user_docker_container(user_dockerfile_abs, user_dockerfile, output_folder_abs, project_root)
+    mounted_data_abs = os.path.join(project_root, mounted_data)
+
+    container_id = container.run_user_docker_container(user_dockerfile_abs, user_dockerfile, output_folder_abs, mounted_data_abs, project_root)
     print("Container started, collecting environment and package information...")
 
     # Part 1: Extract environment and installed packages as soon as the container is running
